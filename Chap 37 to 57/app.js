6e00f6055dd11a4a4845a200b5d6ec5a2bb17f7b
arsal-farooq <arsal.faroq879@hotmail.com>
2020-07-23 09:35:26 -0700

    deploy complete

---
+++ Chap 37 to 57/app.js
@@ -0,0 +1,232 @@
+// =====================================     CHAPTER NO 38  TO  42 ================================================================
+
+
+
+// ================== TASK 1 ====================//
+
+
+
+
+// function raised(){
+//     var a = +prompt("Enter first number");
+//     var b = +prompt("Enter second number")
+//     return Math.pow(a,b)
+// }
+
+// alert (raised())
+
+
+// ==================  TASK 3 =================//
+
+
+// function calc(){
+//  var side1 = +prompt("Enter Side A of triangle") 
+// var side2 =  +prompt("Enter Sid;e B of triangle")
+// var side3 =   +prompt("Enter Side C of triangle") 
+// var s = (side1 + side2 + side3)/2;
+// var area =  Math.sqrt(s*((s-side1)*(s-side2)*(s-side3))).toFixed(1);
+// alert(area);
+// }
+// alert (calc())
+
+
+
+// 
+
+
+
+
+
+
+// ===================== TASK  8 =====================//
+
+
+
+
+// var a = +prompt("Enter distance in Kilo Meter (km)")
+// function Meter(){
+ 
+//       return a * 1000
+// }
+
+// function inch(){
+
+//    return  a * 39370.1
+// }
+
+// function feet(){
+//    return a * 3280.84
+// }
+// function centi(){
+//   return a * 100000
+// }
+
+// document.write(" In Meter :   " ,Meter() , " <br>", " In inches : " , inch() , " <br>", " In Feet :",  feet(), "<br>", " In Centimeter: ", centi())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ====================================  CHAPTER NO 43 TO 48   =====================================//
+
+//  function image(){
+//        document.getElementById("img1").src = "https://images.unsplash.com/photo-1525609004556-c46c7d6cf023?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"
+      
+//  }
+
+//  function old(){
+//      document.getElementById("img1").src = "https://i.ytimg.com/vi/_adJBzQgfsg/maxresdefault.jpg"
+//  }
+
+
+
+
+
+// ===========  TASK 3  ==========//
+
+// function del1(){
+//      document.getElementById("del1").outerHTML ="";
+
+// }
+// function del2(){
+//     document.getElementById("del2").outerHTML ="";
+         
+// }
+
+// function del3(){
+//     document.getElementById("del3").outerHTML ="";
+         
+// }
+// function del4(){
+//     document.getElementById("del4").outerHTML ="";
+         
+// }
+
+
+// ============ TASK  5 ====================/
+
+
+
+//  var a = 0;
+//  var head1 = document.getElementById("number")
+ 
+// function counter(){
+//    a++
+//    head1.innerHTML = a;
+
+    
+// }
+
+// function deccount(){
+//     --a
+//     head1.innerHTML = a
+    
+    
+// }
+
+
+// - ======================= ============ CHAP NO 49 - 52==================================================================================//
+ 
+
+// ========  TASK 1 ===============//
+
+
+// function get(){
+//     var  a =  document.getElementById("one");
+//     var  b =  document.getElementById("two");
+//     var c = document.getElementById("three")
+//      document.write( "Name: ",a.value + " " +  b.value, "<br>" );
+//      document.write ("Email Address: ", c.value)
+// }
+
+
+
+// ==========  TASK 2 =================//
+
+
+//  function expand() { 
+//      var expandedParagraph = "Change is inevitable, Apple plans to reveal iphone 6s Plus and being one of the most iconic products of the company, it comes with many upgrades. With rounded edges, Apple has created a masterpiece, there is no doubt, wrapped in a piece of aluminum the all new 6s ensures a comfort fit. Apple's products are always identical in design and only differs in either size of iphones with increased length & call them Plus or placement of iron pieces around the borders and bezels. Apple's new smart devices could be a real eye-opener for who want a real smartphone, as 6s plus is the phone of the future.";
+//  document.getElementById("para").innerHTML = expandedParagraph;  
+// }
+
+
+
+
+
+// =========== TASK 3==============//
+
+// function del1(){
+//      document.getElementById("del11").outerHTML ="";
+     
+
+// }
+// function edit11 (){
+//     document.getElementById("del11").contentEditable = "true"
+// }
+// function del2(){
+//     document.getElementById("del2").outerHTML ="";
+         
+// }
+// function edit2(){
+//     document.getElementById("del2").contentEditable = "true"
+// }
+
+
+
+// function del3(){
+//     document.getElementById("del3").outerHTML ="";
+         
+// }
+// function edit3(){
+//     document.getElementById("del3").contentEditable = "true"
+// }
+// function del4(){
+//     document.getElementById("del4").outerHTML ="";
+         
+// }
+
+// function edit4(){
+//     document.getElementById("del4").contentEditable = "true"
+// }
+
+
+// =================================================== CHAPTER NO 53 - 58 =======================================================//
+
+
+
+// //===================   TASK NO 2 =============================//
+
+
+// function big(){
+//    var para = document.getElementById("para")
+//    para.style.fontSize =" 30px"
+
+// }
+
+// function small(){
+//     var para =  document.getElementById("para")
+//     para.style.fontSize = " 20px"
+// }
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
---
+++ Chap 58 to 67/app.js
@@ -0,0 +1,64 @@
+//    =================================   CHAPTER NO 58 TO 57 ===========================================//
+
+// ============  TASK 1 ==================================//
+
+
+
+
+
+// ---------  PART 1 ---------------//
+
+
+// var a = document.getElementById("main-content");
+
+// =========== PART 2 --------------
+
+
+// console.log(document.childNodes[1].childNodes[2].childNodes[3].childNodes[5])
+
+
+
+
+
+
+
+// ============================  TASK 2 ===================//
+
+
+
+// ---------------- PART 1----------------//
+
+// var a = document.getElementById('main-content');
+// console.log(a.nodeType);
+
+
+// -------------- PART 2 -----------------//
+
+//  var b = document.getElementById("last-name");
+//   console.log(b.nodeType)
+//   console.log(b.childNodes[2])
+
+
+
+// ============ PART  4 --------------//
+
+
+
+// var c = document.getElementById("main-content");
+//   console.log(c.firstChild)
+//   console.log(c.lastChild)
+
+
+
+//--------------------- TASk 5 -----------//
+
+//   var d = document.getElementById("form-content");
+//     console.log(d.nextSibling);
+//     console.log(d.previousSibling)
+
+
+//==================   TASK 6 --------------//
+
+// var e = document.getElementById("email");
+//  console.log(e.parentNode)
+//  console.log(e.nodeType)
